27f0aa4e76507cc3169051c60d94e560bc375b70
Sandra Petkovski <sandrapetkovskifon@gmail.com>
2024-05-29 22:12:17 +0200

    zadatak

---
+++ script.js
@@ -0,0 +1,11 @@
+const oblast = document.getElementById("oblast");
+const vreme = document.getElementById("vreme");
+const input = document.getElementById("gradoviBox");
+const dugmeDodaj = document.getElementById("dodaj");
+const dugmeZavrsi = document.getElementById("zavrsiIgru");
+const option = document.createElement("option");
+const listaIzabranihGradova = document.getElementById("ubaceniGradovi");
+let listaGradova=[];
+
+
+
